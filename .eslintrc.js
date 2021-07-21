0f791b4ccc4bd3c9f3d5a26a046a24aff86d0966
Benjamin Tan <benjamin@dev.ofcr.se>
2021-07-21 22:29:40 +0800

    ESLint: display Prettier warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,5 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
-  rules: { "prettier/prettier": 0 },
+  rules: { "prettier/prettier": "warn" },
 };
