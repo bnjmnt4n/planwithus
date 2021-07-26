43162075c240759d3d6e28ab7c1b0e7880f7023d
Benjamin Tan <benjamin@dev.ofcr.se>
2021-07-26 21:18:37 +0800

    CheckedPlanItem: increase button margin

--- src/CheckedPlanItem.tsx
+++ src/CheckedPlanItem.tsx
@@ -14,6 +14,7 @@ import { Button, makeStyles, Paper, Tooltip } from "@material-ui/core";
 const useStyles = makeStyles((theme) => ({
   button: {
     marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
   },
 }));
 
